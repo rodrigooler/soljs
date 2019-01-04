1c6ad72955c5d04cef0874f7428f0d375ecd81e9
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2019-01-04 09:34:46 -0200

    improve src/types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,21 @@
 export const getTypeSolidity = value => {
-    if (typeof value === "string") return "string";
-    if (typeof value === "number") return "uint";
-  };
-  
+  if (value instanceof Array) {
+    switch (typeof value) {
+      case 'string':
+        return 'string[]'
+      case 'number':
+        return 'uint[]'
+      case 'boolean':
+        return 'bool[]'
+    }
+  }
+
+  switch (typeof value) {
+    case 'string':
+      return 'string'
+    case 'number':
+      return 'uint'
+    case 'boolean':
+      return 'bool'
+  }
+}
