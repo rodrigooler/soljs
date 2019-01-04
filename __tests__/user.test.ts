fbbcca7881d56b5dc946ba89817cb2883bb20a46
Rodrigo Oler <roodrigoprogrammer@gmail.com>
2019-01-04 10:59:29 -0200

    add glob

--- __tests__/user.test.ts
+++ __tests__/user.test.ts
@@ -8,7 +8,7 @@ describe('Test solidity file', () => {
   test('should check that you have the pragma', () => {
     expect(lines[0]).toBe('pragma solidity ^0.5.0;')
   })
-  test('should verify that you have a contract name', () => {
+  test('should check that have a contract name', () => {
     expect(lines[2]).toBe('contract User {')
   })
   test('should check whether the variables with types', () => {
